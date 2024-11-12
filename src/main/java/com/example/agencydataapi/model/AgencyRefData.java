aaccd5ef6c1e76255668dd2e78e2c72725e5ab38
Aktar <apaktar@gmail.com>
2024-11-12 13:26:33 +0600

    Initial commit for AgencDatayApi project

---
+++ src/main/java/com/example/agencydataapi/model/AgencyRefData.java
@@ -0,0 +1,20 @@
+package com.example.agencydataapi.model;
+public class AgencyRefData {
+    private String name;
+    private String code;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setId(String name) {
+    }
+}
